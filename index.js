2bd1150ac26080440b44d267967f05dd0e94bad3
13thThief <manpreet.plus@gmail.com>
2018-08-05 01:42:14 +0530

    Revamped

---
+++ index.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let ora = require('ora');
+let spinner = ora('Fetching version...');
+
+spinner.start();
+
+let cheerio = require('cheerio');
+let request = require('request');
+
+const NODE_URL = 'https://nodejs.org/en/';
+const URL = require('url').parse(NODE_URL);
+
+const options = {
+  url: URL
+};
+
+function fire(url) {
+  request(options, function (err, res, html) {
+    spinner.stop();
+      if (err) {
+        console.log(`${ URL.hostname } is unreachable OR No Internet connection`);
+      }
+      else if (res.statusCode === 200) {
+        let $ = cheerio.load(html);
+        const latest = $('.home-downloadblock').eq(1).children('a').attr('data-version');
+        if(process.version === latest){
+          console.log('No change');
+        } else {
+          console.log('Need to update to: ', latest);
+        }
+      }
+  });
+}
+
+fire(NODE_URL)
